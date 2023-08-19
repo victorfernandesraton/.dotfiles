54b4a06e96d7bab0a0c3bc21fb88fbe5180cbb25
Victor Raton <vfbraton@gmail.com>
2023-08-19 02:49:50 -0300

    improve: nvim is not transluced

--- nvim/after/plugin/rosepine.lua
+++ nvim/after/plugin/rosepine.lua
@@ -2,7 +2,5 @@ function ApplyRosePine()
 	color = color or 'rose-pine'
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-	vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
 end
 ApplyRosePine()
