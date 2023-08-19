797c8c3af5e7b80ac3e7e3dd04b0d4b8327b093a
Victor Raton <vfbraton@gmail.com>
2023-08-19 01:46:11 -0300

    init: statusbar

--- nvim/after/plugin/neotree.lua
+++ nvim/after/plugin/neotree.lua
@@ -1,3 +1,11 @@
 require("neo-tree").setup({
     hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+
+    filesystem = {
+        filtered_items = {
+            visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+            hide_dotfiles = false,
+            hide_gitignored = true,
+        },
+    }
 })
