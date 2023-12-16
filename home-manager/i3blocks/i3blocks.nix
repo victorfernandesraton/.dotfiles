fa0eddbf43be08d41f7bae6ad24fce430fc6b771
Victor Raton <vfbraton@gmail.com>
2023-12-16 13:10:39 -0300

    feat: improve files to using nix internal instead of expose as a config

--- home-manager/i3blocks/i3blocks.nix
+++ home-manager/i3blocks/i3blocks.nix
@@ -1,15 +1,48 @@
 
 
 {pkgs, ...}:
-
+let 
+  BASE_DIR = ".config/i3blocks";
+  SCRIPT_DIR = "$HOME/${BASE_DIR}";
+in 
 {
     home = {
         file = {
-            ".config/i3blocks/config".source = ./config;
-            ".config/i3blocks/battery.sh".source = ./battery.sh;
-            ".config/i3blocks/disk_usage".source = ./disk_usage;
-            ".config/i3blocks/cpu_usage".source = ./cpu_usage;
-            ".config/i3blocks/memory".source = ./memory;
+            "${BASE_DIR}/battery.sh".source = ./battery.sh;
+            "${BASE_DIR}/disk_usage".source = ./disk_usage;
+            "${BASE_DIR}/cpu_usage".source = ./cpu_usage;
+            "${BASE_DIR}/memory".source = ./memory;
+            "${BASE_DIR}/config".text = ''
+                  [battery]
+                  command=sh ${SCRIPT_DIR}/battery.sh
+                  interval=10
+
+                  [cpu_usage]
+                  command=perl ${SCRIPT_DIR}/cpu_usage 
+                  interval=10
+                  LABEL=CPU 
+
+                  [memory_ram]
+                  command=sh ${SCRIPT_DIR}/memory
+                  label=MEM 
+                  interval=30
+
+                  [time]
+                  command=date +"%a %d/%m/%Y %H:%m"
+                  interval=10
+
+                  [caps-lock]
+                  command=xset -q | grep Caps | awk '{ print $2, $3, $4 }'
+                  interval=1
+
+                  [setxkbmap]
+                  command=setxkbmap -print | awk -F"+" '/xkb_symbols/ {print $2}'
+                  interval=1
+
+                  [volume]
+                  command=echo " $(amixer sget Master | awk -F"[][]" '/Left:/ { print $2 }')"
+                  interval=1
+            '';
         };
     };
 }
