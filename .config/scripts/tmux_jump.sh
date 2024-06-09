fef0fcbb4f70b3dad1a3806a37711f5812aee629
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-06-09 09:56:42 -0300

    feat: add script in repo

---
+++ .config/scripts/tmux_jump.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+FZF_RESULT=$(fd -H -td -d 2 . $HOME/git  | fzf)
+
+
+# get folder name
+FOLDER=$(basename $FZF_RESULT)
+
+# lookup tmux session name
+SESSION=$(tmux list-sessions | grep $FOLDER | awk '{print $1}')
+SESSION=${SESSION//:/}
+
+# check if variable is empty 
+# if not currently in tmux
+if [ -z "$TMUX" ]; then
+  # tmux is not active
+  echo "is not tmux"
+  if [ -z "$SESSION" ]; then
+    # session does not exist
+    echo "session does not exist"
+    # create session
+    tmux new-session -s $FOLDER -c $FZF_RESULT
+  else
+    # session exists
+    echo "session exists"
+    # attach to session
+    tmux attach -t $SESSION
+  fi
+else
+  # tmux is active
+  echo "is tmux"
+  if [ -z "$SESSION" ]; then
+    # session does not exist
+    echo "session does not exist"
+    # jump to directory
+    cd $FZF_RESULT
+    # create session
+    tmux new-session -d -s $FOLDER -c $FZF_RESULT
+    # attach to session
+    tmux switch-client -t $FOLDER
+  else
+    # session exists
+    echo "session exists"
+    # attach to session
+    # switch to tmux session
+    tmux switch-client -t $SESSION
+  fi
+fi
